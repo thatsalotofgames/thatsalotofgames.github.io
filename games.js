ec70f713e4cec9b3e7a5a17f2221cef920713c0f
thatsalotofgames <nguyenduyalex+@gmail.com>
2025-10-25 11:59:36 -0400

    games

--- games.js
+++ games.js
@@ -300,6 +300,11 @@ const games = [
     html: "games/planet life/index.html",
   },
   {
+    name: "Poooooool",
+    image: "games/poooooool/logo.png",
+    html: "games/poooooool/index.html",
+  },
+  {
     name: "Protektor",
     image: "games/protektor/logo.png",
     html: "games/protektor/index.html",
