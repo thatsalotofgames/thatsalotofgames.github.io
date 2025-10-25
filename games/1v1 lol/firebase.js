19e6ddf2f9269fb842bbb11ea1fc92173bd8123d
thatsalotofgames <nguyenduyalex+@gmail.com>
2025-10-25 19:43:49 -0400

    google api

--- games/1v1 lol/firebase.js
+++ games/1v1 lol/firebase.js
@@ -16,7 +16,7 @@ function initializeFireBase() {
 function initializeFireBaseDev() {
   // Your web app's Firebase configuration
   var firebaseConfig = {
-    apiKey: "AIzaSyANZ0SDhqoc62msSooQFs3SEb4XbC7gvk4",
+    apiKey: "",
     authDomain: "dev.1v1.lol",
     databaseURL: "https://dev1v1.firebaseio.com",
     projectId: "dev1v1",
